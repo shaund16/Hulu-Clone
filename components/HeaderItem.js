fe27f4c872dc59cd76ecf5df9995a730b22dcfeb
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-19 15:55:34 -0600

    style the icons

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const HeaderItem = () => {
+const HeaderItem = ({ Icon, title }) => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white'>
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className='opacity-0 group-hover:opacity-100 tracking-widest'>
+        {title}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderItem
+export default HeaderItem;
