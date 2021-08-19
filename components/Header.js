496765bfe47167d5d2c19d7decefdab6cb837925
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-19 15:55:24 -0600

    complete the styles for the main title and icons

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,29 @@
 import Image from 'next/image';
 import React from 'react';
 import HeaderItem from './HeaderItem';
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
 
 const Header = () => {
   return (
-    <header className=''>
-    <div>
-      <HeaderItem title='HOME' Icon={} />
-    </div>
+    <header className='flex flex-col md:flex-row m-5 justify-between items-center h-auto'>
+      <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
       <Image
-        className='object-contain'
-        src='https://links.papareact.com/ua6'
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
         width={200}
         height={100}
       />
