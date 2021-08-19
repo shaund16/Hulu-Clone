bfb335e65eb394e8326dc0c59179d3f5ec2cd1b9
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-19 14:13:10 -0600

    add logo in the header component with src, height and width

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const Header = () => {
   return (
-    <header>
-      
+    <header className=''>
+      <Image
+        className='object-contain'
+        src='https://links.papareact.com/ua6'
+        width={200}
+        height={100}
+      />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
