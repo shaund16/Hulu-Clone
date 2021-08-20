6951d497ea47f0e0e39381f6794145b236b46f6e
Shaun <Shaun@Shauns-MacBook-Pro.local>
2021-08-19 19:30:08 -0600

    add some styles to the thumbnail, h2 features and icons

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
-import { ThumbDownIcon } from '@heroicons/react/outline';
+import { ThumbUpIcon } from '@heroicons/react/outline';
 
 const Thumbnail = ({ result }) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
   return (
-    <div>
+    <div className='group'>
       <Image
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
@@ -16,11 +16,13 @@ const Thumbnail = ({ result }) => {
         width={1920}
       />
       <div className='p-2'>
-        <p className="truncate max-w-md">{result.overview}</p>
-        <h2>
-          {result.title || result.original_name}
-        </h2>
-        <p></p>
+        <p className='truncate max-w-md'>{result.overview}</p>
+        <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>{result.title || result.original_name}</h2>
+        <p className='flex items-center opacity-0 group-hover:opacity-100'>
+          {result.media_type && `${result.media_type} •`}{' '}
+          {result.release_date || result.first_air_date} •{' '}
+          <ThumbUpIcon className='h-5 mx-2' /> {result.vote_count}
+        </p>
       </div>
     </div>
   );
